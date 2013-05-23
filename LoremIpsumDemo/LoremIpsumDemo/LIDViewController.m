47950617f385458d763812d4285aef803bce8e87
Nazar Kanaev <nazar.kanaev@gmail.com>
2013-05-23 16:40:16 +0600

    demo code cleanup

--- LoremIpsumDemo/LoremIpsumDemo/LIDViewController.m
+++ LoremIpsumDemo/LoremIpsumDemo/LIDViewController.m
@@ -16,20 +16,6 @@
 
 @implementation LIDViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark -
-
 - (IBAction)generateWords:(id)sender {
   LoremIpsum* loremIpsum = [LoremIpsum new];
   int numWords = random() % 150 + 3;
